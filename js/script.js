ee1cd64f752cebd75645e7ddd33bc000104c096a
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-23 09:22:46 +0200

    validation form done

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ const form = document.getElementById("form");
 const inputs = document.getElementsByTagName("input");
 const inputsArray = Array(inputs);
 const button = document.getElementById("intro__button");
+const password = document.getElementById("form__password");
 
 button.addEventListener("click", function (event) {
-  event.preventDefault();
   for (let input of inputs) {
-    console.log(input.checkValidity());
+    if (input.checkValidity()) {
+      input.classList.remove("error");
+      input.nextElementSibling.classList.remove("visible");
+    } else {
+      event.preventDefault();
+      input.classList.add("error");
+      input.nextElementSibling.classList.add("visible");
+    }
+
+    if (input.id === "form__password") {
+      if (input.validity.tooShort) {
+        input.nextElementSibling.innerText = "Password must be at least 6 characters long";
+      } else {
+        input.nextElementSibling.innerText = "Password cannot be empty";
+      }
+    }
   }
 });
-
-function allValid(el) {}
