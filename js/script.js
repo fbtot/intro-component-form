c0c3f6d7ecfe7c8346815bf7f7c55755925f7133
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-23 07:29:09 +0200

    working on checkValidity

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 const form = document.getElementById("form");
 const inputs = document.getElementsByTagName("input");
+const inputsArray = Array(inputs);
+const button = document.getElementById("intro__button");
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  for (let input of inputs) {
+    console.log(input.checkValidity());
+  }
+});
+
+function allValid(el) {}
